4302207e06a5431b8a73318935b07506bfd82ad2
tomhayesbu <79951072+tomhayesbu@users.noreply.github.com>
2021-04-27 12:35:15 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -88,25 +88,6 @@ fadeons.forEach(fadeon => {
 }); 
 
 
-
-var mybutton = document.getElementById("myBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
