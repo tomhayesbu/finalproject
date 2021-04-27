a6380ea93ca924aeca81b7d44206dd1a0201e87e
tomhayesbu <79951072+tomhayesbu@users.noreply.github.com>
2021-04-27 12:32:32 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -89,23 +89,22 @@ fadeons.forEach(fadeon => {
 
 
 
-rtsBtn = document.getElementById("rtsBtn");
+var mybutton = document.getElementById("myBtn");
 
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction(){
-    if(document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20){
-            rtsBtn.style.display = "block";
-        }
-        else {
-            rtsBtn.style.display = "none";
-        }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-        function topFunction(){
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        }
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 window.onscroll = function() {myFunction()};
